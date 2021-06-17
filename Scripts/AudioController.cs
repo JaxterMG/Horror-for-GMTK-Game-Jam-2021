0ee6f8609d46d95a05eb297bc0629301a3015c6f
Jaxter <eugeny.craevsky@yandex.ru>
2021-06-17 14:28:30 +0300

    Adding files to github

---
+++ Scripts/AudioController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    AudioSource _audiosource;
+    [SerializeField] AudioClip[] _footstepsAudio;
+    [SerializeField] float _waitTime;
+    float timer;
+
+    void Start()
+    {
+        _audiosource = GetComponent<AudioSource>();
+        timer = _waitTime;
+    }
+
+    public void PlayFootsteps()
+    {
+       // timer -= Time.deltaTime;
+      //  if (timer <= 0)
+      // {
+            _audiosource.PlayOneShot(_footstepsAudio[Random.Range(0, _footstepsAudio.Length - 1)]);
+            timer = _waitTime;
+     //   }
+    }
+}
+
---
+++ Scripts/MirrorRidle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MirrorRidle : MonoBehaviour
+{
+    [SerializeField] RawImage _blackSprite;
+    [SerializeField] Sprite _mirrorSprite;
+    [SerializeField] RawImage _mirrorImage;
+    public void OnInteraction()
+    {
+        _mirrorImage.texture = _mirrorSprite.texture;
+        _blackSprite.DOFade(1, 1).OnComplete(() => { _mirrorImage.DOFade(1, 0); StartCoroutine(Wait()); });
+       
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.E));
+        _mirrorImage.DOFade(0, 1).OnComplete(() => _blackSprite.DOFade(0, 1));
+
+    }
+}
